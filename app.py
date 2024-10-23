be8d5ee9c43640f225071710fa729f0f3166f820
Yogesh <yogikhairnar1912@gmail.com>
2024-10-23 18:28:03 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage
+number1 = 5
+number2 = 10
+result = add_numbers(number1, number2)
+
+print("The sum is:", result)
+
